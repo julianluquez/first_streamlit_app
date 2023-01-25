db3aee7c43015e69bafc6dd5aa940eebec96d588
julianluquez <123528417+julianluquez@users.noreply.github.com>
2023-01-25 00:51:00 -0300

    Update streamlit_app.py
    added text and subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+stramlit.text("Omega 3 & Blueberry Oatmeal")
+stramlit.text("Kale, Spinach & Rocker Smoothie")
+stramlit.text("Hard-Boiled Free-Range Egg")
